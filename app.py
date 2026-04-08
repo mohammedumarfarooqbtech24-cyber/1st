c713399854811c1f70d5684c768ce06b4488140b
umar <mohammedumarfarooqbtech24@rvu.edu.in>
2026-04-08 22:45:07 +0530

    Initial commit: Basic Python-Flask-Docker app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python-Flask-Docker app!"
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    return f"Hello from Python-Flask-Docker app! Timestamp: {timestamp}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
